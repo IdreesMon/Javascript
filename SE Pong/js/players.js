890378c60f2238e3761a53e5d5ce6436a307909d
Idrees <idreesmontequiu@Idreess-MacBook-Pro.local>
2024-08-07 13:53:21 -0400

    Update

---
+++ SE Pong/js/players.js
@@ -0,0 +1,13 @@
+//build class for players
+
+class Player{
+
+    constructor()
+    {
+        this.name="idrees";
+        this.score= 0;
+        this.highScore= 0;
+        this.pad=``
+        return this;
+    }
+}
