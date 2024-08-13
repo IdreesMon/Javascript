d17f1afe3923d67cfe665f9ce1ab499b68a80586
Idrees <idreesmontequiu@Idreess-MacBook-Pro.local>
2024-08-13 14:51:27 -0400

    Pong update

--- SE Pong/js/pong.js
+++ SE Pong/js/pong.js
@@ -62,89 +62,136 @@ ball.color = 'white'
 
 function main() {
     //erases the canvas
-    ctx.clearRect(0, 0, c.width, c.height)
+    ctx.clearRect(0, 0, c.width, c.height);
 
-    //p1 accelerates when key is pressed 
-    if (keys[`w`]) {
-        pad[0].vy += -pad[0].force
+    //Loop through each paddle
+    for (let i = 0; i < pad.length; i++) {
+        // Player controls
+        if (i === 0) { // Player 1
+            if (keys[`w`]) pad[i].vy += -pad[i].force;
+            if (keys[`s`]) pad[i].vy += pad[i].force;
+        } else if (i === 1) { // Player 2
+            if (keys[`o`]) pad[i].vy += -pad[i].force;
+            if (keys[`p`]) pad[i].vy += pad[i].force;
+        }
+
+
+        //apply friction
+
+        pad[i].vy *= fy;
+
+        //Move the paddle
+        pad[i].move();
+
+
+        //Paddle collision with the canvas borders
+        if (pad[i].y < 0 + pad[i].h / 2){
+            pad[i].y = 0 + pad[i].h / 2;
+        }
+        if (pad[i].y > c.height - pad[i].h / 2) {
+            pad[i].y = c.height - pad[i].h / 2;
+        }
+
+
+        // Ball collision with the paddles
+        if (ball.collide(pad[i])) {
+            if (i === 0) {
+                ball.x = pad[i].x + pad[i].w / 2 + ball.w / 2;
+            } else {
+                ball.x = pad[i].x - pad[i].w / 2 - ball.w / 2;
+            }
+            ball.vx = -ball.vx;
+        }
+
+        //draw the paddle
+
+        pad[i].draw();
+
     }
 
-    if (keys[`s`]) {
-        pad[0].vy += pad[0].force
-    }
-    //p2 accelerates when key is pressed
-    if (keys[`o`]) {
-        pad[1].vy += -pad[1].force
-    }
+    //Ball movement
 
-    if (keys[`p`]) {
-        pad[1].vy += pad[1].force
-    }
-    //applies friction
-    pad[0].vy *= fy;
-    pad[1].vy *= fy;
-
-    //player movement
-    pad[0].move();
-    pad[1].move();
-
-    //ball movement
     ball.move();
 
-
-    //p1 collision
-    if (pad[0].y < 0 + pad[0].h / 2) {
-        pad[0].y = 0 + pad[0].h / 2
-    }
-    if (pad[0].y > c.height - pad[0].h / 2) {
-        pad[0].y = c.height - pad[0].h / 2
-    }
-    //p2 collision 
-    if (pad[1].y < 0 + pad[1].h / 2) {
-        pad[1].y = 0 + pad[1].h / 2
-    }
-    if (pad[1].y > c.height - pad[1].h / 2) {
-        pad[1].y = c.height - pad[1].h / 2
-    }
-
-    //ball collision 
-    if (ball.x < 0) {
-        player[1].score++
-        player[1].updateScore()
-        ball.x = c.width / 2
-        ball.y = c.height / 2
-    }
-
-    if (ball.x >= c.width) {
-        player[0].score++
-        player[0].updateScore()
-        ball.x = c.width / 2
-        ball.y = c.height / 2
-        ball.x = c.width
-        ball.vx = -ball.vx
-    }
-
     if (ball.y < 0) {
-        ball.y = 0
-        ball.vy = -ball.vy
+        ball.y = 0;
+        ball.vy = -ball.vy;
     }
 
     if (ball.y > c.height) {
-        ball.y = c.height
-        ball.vy = -ball.vy
+        ball.y = c.height;
+        ball.vy = -ball.vy;
     }
 
-    for (let i = 0; i < pad.length; i++) {
-        if (ball.collide(pad[i])) {
-            if (i === 0) {
-                ball.x = pad[i].x + pad[i].w / 2 + ball.w / 2
-            } else {
-                ball.x = pad[i].x - pad[i].w / 2 - ball.w / 2
-            }
-
-            ball.vx = -ball.vx;
-        }
+    // ball goes out on left player 2 scored
+    if (ball.x < 0) {
+        player[1].score++;
+        player[1].updateScore();
+        ball.x = c.width / 2;
+        ball.y = c.height / 2;
+        ball.vx = -ball.vx;
     }
+
+    // ball goes out on right player 1 scored
+
+
+    if (ball.x >= c.width) {
+        player[0].score++;
+        player[0].updateScore();
+        ball.x = c.width / 2;
+        ball.y = c.height /  2;
+        ball.vx = -ball.vx;
+    }
+
+      //draw the objects
+    //   pad[0].draw();
+    //   pad[1].draw();
+      ball.draw();
+
+}
+
+
+
+
+
+    //ball collision 
+    // if (ball.x < 0) {
+    //     player[1].score++
+    //     player[1].updateScore()
+    //     ball.x = c.width / 2
+    //     ball.y = c.height / 2
+    // }
+
+    // if (ball.x >= c.width) {
+    //     player[0].score++
+    //     player[0].updateScore()
+    //     ball.x = c.width / 2
+    //     ball.y = c.height / 2
+    //     //ball.x = c.width
+    //     ball.vx = -ball.vx
+    // }
+
+    // if (ball.y < 0) {
+    //     ball.y = 0
+    //     ball.vy = -ball.vy
+    // }
+
+    // if (ball.y > c.height) {
+    //     ball.y = c.height
+    //     ball.vy = -ball.vy
+    // }
+
+    // for (let i = 0; i < pad.length; i++) {
+    //     if (ball.collide(pad[i])) {
+    //         if (i === 0) {
+    //             ball.x = pad[i].x + pad[i].w / 2 + ball.w / 2
+    //         } else {
+    //             ball.x = pad[i].x - pad[i].w / 2 - ball.w / 2
+    //         }
+
+    //         ball.vx = -ball.vx;
+    //     }
+    // }
 
     // p1 with ball collision
     // if (ball.collide(pad[0])) {
@@ -160,13 +207,46 @@ function main() {
     // }
 
 
-    //draw the objects
-    pad[0].draw();
-    pad[1].draw();
-    ball.draw();
+    //p1 accelerates when key is pressed 
+    // if (keys[`w`]) {
+    //     pad[0].vy += -pad[0].force
+    // }
+
+    // if (keys[`s`]) {
+    //     pad[0].vy += pad[0].force
+    // }
+    // //p2 accelerates when key is pressed
+    // if (keys[`o`]) {
+    //     pad[1].vy += -pad[1].force
+    // }
+
+    // if (keys[`p`]) {
+    //     pad[1].vy += pad[1].force
+    // }
+
+
+    // //applies friction
+    // pad[0].vy *= fy;
+    // pad[1].vy *= fy;
+
+    //player movement
+    // pad[0].move();
+    // pad[1].move();
 
 
 
 
-}
-
+      // //p1 collision
+    // if (pad[0].y < 0 + pad[0].h / 2) {
+    //     pad[0].y = 0 + pad[0].h / 2
+    // }
+    // if (pad[0].y > c.height - pad[0].h / 2) {
+    //     pad[0].y = c.height - pad[0].h / 2
+    // }
+    // //p2 collision 
+    // if (pad[1].y < 0 + pad[1].h / 2) {
+    //     pad[1].y = 0 + pad[1].h / 2
+    // }
+    // if (pad[1].y > c.height - pad[1].h / 2) {
+    //     pad[1].y = c.height - pad[1].h / 2
+    // }
